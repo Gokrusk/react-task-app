7450ae0569e5a4b431df1dcd44ec813e012df2dd
gokrusk <nigelljama@gmail.com>
2023-02-15 22:34:37 -0500

    task container style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import './App.css'
+import image from './images/logoico.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <div className='task-app'>
+        <div className='espoch-logo'>
+          <img src={image} alt='Logo ESPOCH' className='espochLogo'/>
+        </div>
+      </div>
+      <div className='task-list-cont'>
+        <h1>My Tasks</h1>
+      </div>
     </div>
   )
 }
