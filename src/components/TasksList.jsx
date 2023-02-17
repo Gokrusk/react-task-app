6e616cc17b4cb2a86dd71054da4aceb4f13e4a0d
gokrusk <nigelljama@gmail.com>
2023-02-16 19:26:32 -0500

    Task's list component

---
+++ src/components/TasksList.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { TaskForm } from './TaskForm';
+import { Task } from './Task';
+import '../styles/Task.css';
+import '../styles/TaskForm.css';
+import '../styles/TaskList.css';
+
+export const TaskList = () => {
+  const [tasks, setTasks] = useState([])
+
+  const addTask = task =>{
+    if(task.text.trim()){
+      tasks.text = task.text.trim()
+      const updatedTasks = [task, ...tasks] //adding at the begining
+      setTasks(updatedTasks)
+    }
+  }
+
+  const deleteTask = id => {
+    const updatedTasks = tasks.filter(task => task.id !== id)
+    setTasks(updatedTasks)
+  }
+
+  return (
+    <>
+      <TaskForm onSubmit={addTask}/>
+      <div className='task-list-cont'>
+        {
+          tasks.map((task) =>
+            <Task 
+              key={task.id}
+              id={task.id}
+              text={task.text}
+              done = {false}
+            />
+          )   
+        }
+      </div>
+    </>
+  );
+};
