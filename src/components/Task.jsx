50574d508a139fab7fe92d7a47524e157c44e830
gokrusk <nigelljama@gmail.com>
2023-02-16 22:02:09 -0500

    task and taskslist component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,15 +4,10 @@ import { AiOutlineCloseCircle } from 'react-icons/ai';
 export const Task = ({ id, text, done, doneTask, deleteTask }) => {
   return (
     <div className={done ? 'task-cont done' : 'task-cont'}>
-      <div 
-        className='task-text'
-        onClick={() => doneTask(id)}
-      >
+      <div className='task-text' onClick={() => doneTask(id)}>
         {text}
       </div>
-      <div className='task-icon-cont'
-        onClick={() => deleteTask(id)}
-      >
+      <div className='task-icon-cont' onClick={() => deleteTask(id)}>
         <AiOutlineCloseCircle className='task-icon' />
       </div>
     </div>
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -6,35 +6,45 @@ import '../styles/TaskForm.css';
 import '../styles/TaskList.css';
 
 export const TaskList = () => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
 
-  const addTask = task =>{
-    if(task.text.trim()){
-      tasks.text = task.text.trim()
-      const updatedTasks = [task, ...tasks] //adding at the begining
-      setTasks(updatedTasks)
+  const addTask = (task) => {
+    if (task.text.trim()) {
+      tasks.text = task.text.trim();
+      const updatedTasks = [task, ...tasks]; //adding at the begining
+      setTasks(updatedTasks);
     }
-  }
+  };
 
-  const deleteTask = id => {
-    const updatedTasks = tasks.filter(task => task.id !== id)
-    setTasks(updatedTasks)
-  }
+  const deleteTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const doneTask = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        task.done = !task.done;
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
   return (
     <>
-      <TaskForm onSubmit={addTask}/>
+      <TaskForm onSubmit={addTask} />
       <div className='task-list-cont'>
-        {
-          tasks.map((task) =>
-            <Task 
-              key={task.id}
-              id={task.id}
-              text={task.text}
-              done = {false}
-            />
-          )   
-        }
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            done={task.done}
+            doneTask={doneTask}
+            deleteTask={deleteTask}
+          />
+        ))}
       </div>
     </>
   );
