acbdf2c9095a33312fb82b489dea93f3ff2e0843
gokrusk <nigelljama@gmail.com>
2023-02-16 19:26:18 -0500

    Task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+
+export const Task = ({ id, text, done, doneTask, deleteTask }) => {
+  return (
+    <div className={done ? 'task-cont done' : 'task-cont'}>
+      <div 
+        className='task-text'
+        onClick={() => doneTask(id)}
+      >
+        {text}
+      </div>
+      <div className='task-icon-cont'
+        onClick={() => deleteTask(id)}
+      >
+        <AiOutlineCloseCircle className='task-icon' />
+      </div>
+    </div>
+  );
+};
